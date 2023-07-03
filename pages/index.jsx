c1eba2df785b0f63a2dd7a022f37de237308a4b4
Darius Garcia <dariusgarcia888@gmail.com>
2023-07-03 15:18:20 -0700

    changed home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,53 +6,38 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className=''>
+    <main className='pt-12'>
       {/* Header CTA */}
       <div className=' '>
         <div className='mx-auto max-w-7xl px-6 py-12 sm:py-24 lg:flex lg:items-center lg:gap-x-10 lg:px-8'>
           <div className='mx-auto max-w-2xl lg:mx-0 lg:flex-auto'>
-            <div className='flex '>
+            <div className='flex w-full'>
               <div className='flex flex-col w-full'>
-                <div className=' lg:mt-0 mb-6 lg:flex-shrink-0 lg:flex-grow flex justify-center md:flex-none md:justify-start'>
+                {/* <div className=' lg:mt-0 mb-6 lg:flex-shrink-0 lg:flex-grow flex justify-center md:flex-none md:justify-start'>
                   <Image
                     src='/mainLogo.png'
                     width={300}
                     height={300}
                     alt='logo'
                   />
-                </div>
-                <div className='relative flex items-center justify-start gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
-                  <span className='font-semibold text-green-700 w-full'>
-                    A burger joint.
-                  </span>
-                  <span
-                    className='h-4 w-px bg-gray-900/10'
-                    aria-hidden='true'
-                  />
-                </div>
+                </div> */}
               </div>
             </div>
             <h1 className='mt-10 text-4xl font-bold tracking-tight sm:text-6xl'>
               Doobie Burgers
             </h1>
-            <p className='mt-6 text-lg leading-8 text-gray-500'>
+            <div className='flex items-center justify-center w-full gap-x-4 rounded-full px-4 py-1 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
+              <span className='mt-4 w-full font-semibold text-center'>
+                A burger joint.
+              </span>
+              <span className='h-4 w-px bg-gray-900/10' aria-hidden='true' />
+            </div>
+            <p className='mt-6 text-lg leading-8 text-gray-400'>
               Burgers are unique and always have a story to tell. Come along
               with as we tour Los Angeles letting the World see our story come
               into pictue.
             </p>
             <div className='mt-10 '>
-              <div className='flex  items-center gap-x-6'>
-                <a
-                  href='/menu'
-                  className='rounded-md bg-green-600 px-3.5 py-2.5 w-24 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 transition ease-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 '>
-                  Menu
-                </a>
-                <a
-                  href='/contact'
-                  className='text-sm font-semibold leading-6 hover:opacity-75 transition ease-out '>
-                  Contact us <span aria-hidden='true'>→</span>
-                </a>
-              </div>
               <p className='flex justify-start items-start gap-x-1 w-full mt-12 italic text-sm text-gray-400'>
                 Est. Hollywood 2021
               </p>
@@ -64,7 +49,7 @@ export default function Home() {
         </div>
         <Testimonial />
         <LandingCTA />
-        <ContactInfo />
+        {/* <ContactInfo /> */}
       </div>
     </main>
   )
